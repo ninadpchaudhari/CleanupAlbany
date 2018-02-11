e33c8738a01f75a8735c33e988a0f2128758af4e
Laptop1/Ninad P.Chaudhari <nchaudhari@albany.edu>
2018-02-10 21:07:41 -0500

    Added DB details

--- src/main/java/com/example/controllers/RegistrationController.java
+++ src/main/java/com/example/controllers/RegistrationController.java
@@ -4,12 +4,16 @@ package com.example.controllers;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.models.Student;
 import com.example.service.StudentService;
@@ -32,4 +36,11 @@ public class RegistrationController {
 		return "index";
 	}
 	
+	@PostMapping(value="/registerPersonDevice")
+	@ResponseBody
+	public ResponseEntity<?> updateRestaurantProfile(@RequestParam Map<String, Object> model, ModelMap otherModel){
+		
+		//return ResponseEntity.badRequest().build();
+		return ResponseEntity.accepted().build();
+	}
 }
