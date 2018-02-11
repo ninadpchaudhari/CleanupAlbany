949e98d7190b48217c58a964dc5ed38d0f99470b
Laptop1/Ninad P.Chaudhari <nchaudhari@albany.edu>
2018-02-11 01:59:25 -0500

    Create new file

--- src/main/java/com/example/service/StorageService.java
+++ src/main/java/com/example/service/StorageService.java
@@ -14,10 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 public class StorageService {
 
 	public boolean saveImage(Long IssueId,MultipartFile image) {
-		Path resourceDirectory = Paths.get("src/main/webapp/resources/issue_"+IssueId.toString()+".png");
+		Path resourceDirectory = Paths.get("src/main/webapp/resources");
 		try {
 			File newFile = new File(resourceDirectory.toAbsolutePath().toString());
 			newFile.mkdirs();
+			newFile = new File(resourceDirectory.toAbsolutePath().toString() + '/' + image.getOriginalFilename());
 			image.transferTo(newFile);
 		}catch(Exception e) {
 			e.printStackTrace();
