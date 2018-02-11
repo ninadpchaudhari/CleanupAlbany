e8a8be2a4eaa1d42156e295ac19d5650b6315cf8
Laptop1/Ninad P.Chaudhari <nchaudhari@albany.edu>
2018-02-11 08:32:44 -0500

    dede

--- src/main/java/com/example/service/NotificationService.java
+++ src/main/java/com/example/service/NotificationService.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.http.entity.StringEntity;
@@ -33,15 +34,17 @@ public class NotificationService {
 			   httpHeaders.set("Authorization", "key=" + "AAAA68-dUKE:APA91bGs9xgjmLRdod-m9EeopxYEKoNUD67zHEBOraLU5qpSgghUHQRVL52D5FXi0YVjxOYSiKlPqSsVC9NkQgcxGQAfO2eJ-Ft_0qaH19b2G_ydrUb9U-Qq5R-ecagxgnqLY1O6FRqf");
 			   httpHeaders.set("Content-Type", "application/json");
 			   JSONObject msg = new JSONObject();
-			   JSONObject json = new JSONObject();
+			   HashMap<String,String> myMap = new HashMap<String,String>();
+			   myMap.put("to", fcmtoken);
+			   JSONObject json = new JSONObject(myMap);
 
 			   msg.put("title", "Issue");
 			   msg.put("detail", issueId);
 			   //msg.put("notificationType", "Test");
 
 			   json.put("data", msg);
-			   json.put("to", fcmtoken);
-
+			  // json.put("to", fcmtoken);
+			   System.out.println(json.toString());
 			   HttpEntity<String> httpEntity = new HttpEntity<String>(json.toString(),httpHeaders);
 			   String response = restTemplate.postForObject(androidFcmUrl,httpEntity,String.class);
 			   System.out.println(response);
