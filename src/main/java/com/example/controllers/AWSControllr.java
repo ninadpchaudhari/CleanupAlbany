aa62ba737605486f27b1f997fb0bd64c153d7ab7
Laptop1/Ninad P.Chaudhari <nchaudhari@albany.edu>
2018-02-12 18:36:00 -0500

    fwe

--- src/main/java/com/example/controllers/AWSControllr.java
+++ src/main/java/com/example/controllers/AWSControllr.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -15,6 +16,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectResult;
 
 @Controller
 public class AWSControllr {
@@ -25,8 +27,7 @@ public String renderUpload() {
 	return "upload";
 }
 @PostMapping(value="/file")
-public String function1(@RequestParam("file") MultipartFile image,
-        RedirectAttributes redirectAttributes) {
+public ModelAndView function1(@RequestParam("file") MultipartFile image) {
 	BasicAWSCredentials credentials = new BasicAWSCredentials(
 			  "AKIAI734MTY6LHYHFCNA", 
 			  "hOS6HlfZccWHJr143GENVjMqq9F17tJNw/7w2Ah2"
@@ -46,11 +47,17 @@ public String function1(@RequestParam("file") MultipartFile image,
 		
 		
 		
-		s3client.putObject(
+		PutObjectResult lol = s3client.putObject(
 				  "lolwaimagebucket", 
 				  image.getOriginalFilename(), 
 				  newFile
 				);
+		String srcUrl = "lolwaimagebucket" + ".s3.amazonaws.com" + "/" + image.getOriginalFilename();
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		mv.addObject("srcUrl", srcUrl);
+		return mv;
+		
 	} catch (IllegalStateException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -60,7 +67,14 @@ public String function1(@RequestParam("file") MultipartFile image,
 	}
 	
 
-		return "index";
+		return new ModelAndView("index");
 
 	}
 }
+/*
+ * 
+ * 
+ * https://stackoverflow.com/questions/7933458/url-to-get-a-file-from-amazon-s3
+ * http://www.baeldung.com/aws-s3-java
+ * https://tiffanybbrown.com/2014/09/making-all-objects-in-an-s3-bucket-public-by-default/index.html
+ */
